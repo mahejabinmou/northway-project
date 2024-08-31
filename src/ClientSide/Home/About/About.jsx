350ac7db92b15ad4c8321afcbb2a50031f1d2080
dmemopract <dmemopract@gmail.com>
2024-08-31 02:29:03 -0600

    home commit

--- src/ClientSide/Home/About/About.jsx
+++ src/ClientSide/Home/About/About.jsx
@@ -1,22 +1,82 @@
 import { MdArrowRightAlt } from "react-icons/md";
-import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <div className="sectionGap">
       <div className="chooseUsParent">
-        <div
-          className="relative w-full overflow-hidden chooseUsImgParent"
-          style={{ paddingTop: "56.25%" }}
-        >
-          <iframe
-            className="absolute top-0 left-0 w-full h-full"
-            src="https://www.youtube.com/embed/nYVLmQ0rTtY?si=AZtf4tP-C38tBGF5&rel=0"
-            title="YouTube video player"
-            frameBorder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-          ></iframe>
+        <div>
+          <div
+            className="relative w-full overflow-hidden chooseUsImgParent"
+            style={{ paddingTop: "56.25%" }}
+          >
+            <iframe
+              className="absolute top-0 left-0 w-full h-full"
+              src="https://www.youtube.com/embed/nYVLmQ0rTtY?si=AZtf4tP-C38tBGF5&rel=0"
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowFullScreen
+            ></iframe>
+          </div>
+          <div
+            className=" xl:pt-[24px] xl:gap-x-[20px] 2xl:pt-[24px]   
+          2xl:gap-x-[20px] lg:pt-[24px] lg:gap-x-[10px] md:pt-[24px] md:gap-x-[16px] flex justify-between items-center font-helvetica font-bold"
+          >
+            <div className=" ">
+              <h1
+                className="xl:text-[36px] xl:leading-[43px] 
+              2xl:text-[36px] 2xl:leading-[43px] lg:text-[20px] lg:leading-[20px]
+              md:text-[20px] md:leading-[20px] text-center pb-[5px] font-bold text-[#F6941E]"
+              >
+                800+
+              </h1>
+              <p
+                className="xl:text-[16px]
+          xl:leading-[24px] 2xl:text-[16px]
+          2xl:leading-[24px] lg:text-[12px]
+          lg:leading-[10px] md:text-[12px]
+          md:leading-[14px] text-center"
+              >
+                Global niversity Tips
+              </p>
+            </div>
+            <div className=" text-center">
+              <h1
+                className="xl:text-[36px] xl:leading-[43px] 
+              2xl:text-[36px] 2xl:leading-[43px] lg:text-[20px] lg:leading-[20px]
+              md:text-[20px] md:leading-[20px] text-center pb-[5px] font-bold text-[#F6941E]"
+              >
+                65+
+              </h1>
+              <p
+                className="xl:text-[16px]
+          xl:leading-[24px] 2xl:text-[16px]
+          2xl:leading-[24px] lg:text-[12px]
+          lg:leading-[10px] md:text-[12px]
+         md:leading-[14px] text-center"
+              >
+                Office Across The Globe
+              </p>
+            </div>
+            <div className=" text-center">
+              <h1
+                className="xl:text-[36px] xl:leading-[43px] 
+              2xl:text-[36px] 2xl:leading-[43px] lg:text-[20px] lg:leading-[20px]
+              md:text-[20px] md:leading-[20px] text-center pb-[5px] font-bold text-[#F6941E]"
+              >
+                25+
+              </h1>
+              <p
+                className="xl:text-[16px]
+          xl:leading-[24px] 2xl:text-[16px]
+          2xl:leading-[24px] lg:text-[12px]
+          lg:leading-[10px] md:text-[12px]
+          md:leading-[14px] text-center"
+              >
+                Years Industry Presence
+              </p>
+            </div>
+          </div>
         </div>
 
         <div>
@@ -33,7 +93,10 @@ const About = () => {
             excellence, partner with us, and embark on a transformative journey
             towards success. Contact us now!
           </p>
-          <ul className="chooseUsParaText list-disc xl:pl-5 xl:space-y-2 text-black/[0.8]">
+          <ul
+            className="chooseUsParaText list-disc xl:pl-5 xl:space-y-2
+          2xl:pl-5 2xl:space-y-2 lg:pl-5 lg:space-y-2 md:pl-5 md:space-y-2 text-black/[0.8]"
+          >
             <li>Expert Guidance.</li>
             <li>Seamless Transitions.</li>
             <li>Study Abroad Success.</li>
--- src/ClientSide/Home/HeroSection/HeroSection.jsx
+++ src/ClientSide/Home/HeroSection/HeroSection.jsx
@@ -7,36 +7,21 @@ import Navbar from "../../Shared/Navbar/Navbar";
 import { MdArrowRightAlt } from "react-icons/md";
 
 const HeroSection = () => {
-  const [open, setOpen] = useState(false);
-  const [scrolled, setScrolled] = useState(false);
+  //   useEffect(() => {
+  //     const handleScroll = () => {
+  //       if (window.scrollY > 0) {
+  //         setScrolled(true);
+  //       } else {
+  //         setScrolled(false);
+  //       }
+  //     };
 
-  const handleMenu = () => {
-    setOpen(true);
-  };
+  //     window.addEventListener("scroll", handleScroll);
 
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  const menuClose = () => {
-    setOpen(false);
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
+  //     return () => {
+  //       window.removeEventListener("scroll", handleScroll);
+  //     };
+  //   }, []);
 
   return (
     <div className="relative">
@@ -57,62 +42,87 @@ const HeroSection = () => {
 
       {/* Slider Container big screen*/}
       <div className="  hidden md:block ">
-        <div className="relative ">
+        <div className="relative">
           <div
             style={{
-              backgroundImage: `url(${"https://i.ibb.co/svGKkkx/frame-58-66cecf6f5b40e.webp"})`,
+              backgroundImage: `url("https://i.ibb.co/svGKkkx/frame-58-66cecf6f5b40e.webp")`,
             }}
-            className="
-            bg-no-repeat bg-center bg-cover w-[100%] h-[80vh]   md:h-[70vh] lg:h-[70vh]  xl:h-[100vh] 2xl:h-[100vh]"
+            className="bg-no-repeat bg-center bg-cover w-full h-[914px] flex justify-center items-center"
           ></div>
-          <div className="absolute inset-0 font-helvetica xl:top-[200px] xl:ml-[150px] xl:mr-[733px]">
-            <h1 className="text-white xl:text-[64px] font-bold xl:leading-[77px] xl:pb-[15px]">
-              <span className="text-[#F6941E]">Welcome</span> to Northway Global
-            </h1>
-            <p className="text-white xl:pb-[30px] font-helvetica font-normal xl:text-[24px] xl:leading-[24px]">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
-              tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
-            </p>
-            <button className="homeLargeAppoinMent flex items-center justify-between bg-[#F6941E]">
-              Start Your Journey <MdArrowRightAlt />
-            </button>
+        </div>
+
+        <div
+          className="absolute inset-0 font-helvetica  
+        xl:mt-[200px]  mt-[200px]  md:mt-[150px]  xs:mt-[200px]
+          xl:ml-[150px]  lg:mt-[150px]
+          lg:ml-[60px] md:ml-[80px]  ml-[60px] xs:lg:ml-[150px] "
+        >
+          <h1 className="text-white mr-[583px] xl:mr-[583px]  md:mr-[200px] xs:mr-[583px] text-[60px] xl:leading-[70px] xl:pb-[15px] lg:mr-[300px] lg:leading-[70px] lg:pb-[15px]  font-bold ">
+            <span className="text-[#F6941E]">Welcome</span> to Northway Global
+          </h1>
+          <p className="text-white  text-[16px] font-helvetica font-normal xl:leading-[24px] xl:mr-[655px]  xl:pb-[30px] lg:leading-[24px] lg:mr-[450px]  lg:pb-[30px] md:mr-[250px]  md:leading-[24px]  md:pb-[30px]">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar dapibus leo.
+          </p>
+          <button className="homeLargeAppoinMent flex items-center justify-between bg-[#F6941E]">
+            Start Your Journey <MdArrowRightAlt />
+          </button>
+        </div>
+        <div
+          className="absolute inset-0 font-helvetica 2xl:top-[597px] xl:top-[597px] lg:top-[597px] md:top-[550px]  xl:mx-[150px]   
+         2xl:mx-[150px] lg:mx-[60px] md:mx-[80px]  grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 xl:gap-x-[24px]  2xl:gap-x-[24px] lg:gap-x-[20px]
+         md:gap-x-[10px]"
+        >
+          <div
+            className="bg-white/[0.15] flex justify-center items-center xl:gap-x-[16px] xl:pl-[28px] xl:pr-[8px]  xl:py-[30px] xl:h-[140px]
+            2xl:gap-x-[16px] 2xl:pl-[28px] 2xl:pr-[8px]  2xl:py-[30px] 2xl:h-[140px] lg:gap-x-[16px] lg:pl-[20px] lg:pr-[14px]  lg:py-[30px] lg:h-[140px]
+            md:gap-x-[16px] md:pl-[10px] md:pr-[4px]  md:py-[30px] md:h-[140px]
+          border  border-white/[0.2]  text-white"
+          >
+            <img
+              className="xl:w-[50px] xl:h-[50px] 2xl:w-[50px] 2xl:h-[50px] lg:w-[50px] lg:h-[50px] md:w-[30px] md:h-[30px]"
+              src="https://i.ibb.co/Hx6q9Ns/vector-3-66cf81d0282e5.webp"
+              alt=""
+            />
+            <p className="herocardText">Experienced Consultants</p>
           </div>
 
-          <div className="absolute inset-0 font-helvetica xl:top-[597px] xl:bottom-[177px] xl:mx-[150px] grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-4 md:grid-cols-4 xl:gap-x-[24px]">
-            <div className="bg-white/[0.15] flex items-center xl:gap-x-[16px] xl:px-[28px] xl:py-[34px] xl:w-[267px] xl:h-[140px] border  border-white/[0.2]  text-white">
-              <img
-                className="xl:w-[50px] xl:h-[50px]"
-                src="https://i.ibb.co/Hx6q9Ns/vector-3-66cf81d0282e5.webp"
-                alt=""
-              />
-              <p className="herocardText">Experienced Consultants</p>
-            </div>
+          <div
+            className="bg-white/[0.15]  border-b-[#F6941E] border-b-2 flex justify-center items-center xl:pl-[28px] xl:pr-[8px]  xl:py-[30px] xl:h-[140px]
+            2xl:gap-x-[16px] 2xl:pl-[28px] 2xl:pr-[8px]  2xl:py-[30px] 2xl:h-[140px] lg:gap-x-[16px] lg:pl-[28px] lg:pr-[8px]  lg:py-[30px] lg:h-[140px]
+            md:gap-x-[16px] md:pl-[10px] md:pr-[4px]  md:py-[30px] md:h-[140px] "
+          >
+            <img
+              className="xl:w-[50px] xl:h-[50px] 2xl:w-[50px] 2xl:h-[50px] lg:w-[50px] lg:h-[50px] md:w-[30px] md:h-[30px]"
+              src="https://i.ibb.co/QcBSbWw/frame-1-66cf82241ae84.webp"
+              alt=""
+            />
+            <p className="herocardText text-[#F6941E]">98.8% Success Rate</p>
+          </div>
 
-            <div className="bg-white/[0.15]  border-b-[#F6941E] border-b-2 flex items-center xl:gap-x-[16px] xl:px-[28px] xl:py-[34px] xl:w-[267px] xl:h-[140px]">
-              <img
-                className="xl:w-[50px] xl:h-[50px]"
-                src="https://i.ibb.co/QcBSbWw/frame-1-66cf82241ae84.webp"
-                alt=""
-              />
-              <p className="herocardText text-[#F6941E]">98.8% Success Rate</p>
-            </div>
-
-            <div className="bg-white/[0.15] flex items-center xl:gap-x-[16px]  xl:px-[28px] xl:py-[34px] xl:w-[267px] xl:h-[140px]  border  border-white/[0.2]  text-white">
-              <img
-                className="xl:w-[50px] xl:h-[50px]"
-                src="https://i.ibb.co/kJn5cHY/group-66cf8262e596a.webp"
-                alt=""
-              />
-              <p className="herocardText2">Positive And Timely Results</p>
-            </div>
-            <div className="bg-white/[0.15] flex items-center xl:gap-x-[16px] xl:px-[28px] xl:py-[34px] xl:w-[267px] xl:h-[140px] border  border-white/[0.2]  text-white">
-              <img
-                className="xl:w-[50px] xl:h-[50px]"
-                src="https://i.ibb.co/F8ZfMfM/frame-2-66cf82a790568.webp"
-                alt=""
-              />
-              <p className="herocardText">Seamless Procedures</p>
-            </div>
+          <div
+            className="bg-white/[0.15] flex items-center xl:pl-[28px] xl:pr-[8px]  xl:py-[30px] xl:h-[140px]
+            2xl:gap-x-[16px] 2xl:pl-[28px] 2xl:pr-[8px]  2xl:py-[30px] 2xl:h-[140px] lg:gap-x-[16px] lg:pl-[28px] lg:pr-[8px]  lg:py-[30px] lg:h-[140px]
+            md:gap-x-[16px] md:pl-[10px] md:pr-[4px]  md:py-[30px] md:h-[140px] border  border-white/[0.2]  text-white"
+          >
+            <img
+              className="xl:w-[50px] xl:h-[50px] 2xl:w-[50px] 2xl:h-[50px] lg:w-[50px] lg:h-[50px] md:w-[30px] md:h-[30px]"
+              src="https://i.ibb.co/kJn5cHY/group-66cf8262e596a.webp"
+              alt=""
+            />
+            <p className="herocardText2">Positive And Timely Results</p>
+          </div>
+          <div
+            className="bg-white/[0.15] flex items-center xl:pl-[28px] xl:pr-[8px]  xl:py-[30px] xl:h-[140px]
+            2xl:gap-x-[16px] 2xl:pl-[28px] 2xl:pr-[8px]  2xl:py-[30px] 2xl:h-[140px] lg:gap-x-[16px] lg:pl-[28px] lg:pr-[8px]  lg:py-[30px] lg:h-[140px]
+            md:gap-x-[16px] md:pl-[10px] md:pr-[4px]  md:py-[30px] md:h-[140px] border  border-white/[0.2]  text-white"
+          >
+            <img
+              className="xl:w-[50px] xl:h-[50px] 2xl:w-[50px] 2xl:h-[50px] lg:w-[50px] lg:h-[50px] md:w-[30px] md:h-[30px]"
+              src="https://i.ibb.co/F8ZfMfM/frame-2-66cf82a790568.webp"
+              alt=""
+            />
+            <p className="herocardText">Seamless Procedures</p>
           </div>
         </div>
       </div>
