884dd6bef3fa8e2cfa598087eb56979248f875f2
dmemopract <dmemopract@gmail.com>
2024-09-12 04:36:27 -0600

    glance

--- src/ClientSide/Distenation/Glance/Glance.jsx
+++ src/ClientSide/Distenation/Glance/Glance.jsx
@@ -24,12 +24,14 @@ const Glance = () => {
                 >
                   Course Duration
                 </h3>
-                <p className="pt-[31px] xl:pl-[73px] xl:pr-[63px]  pb-[10px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
-                  4 Years (Bachelor’s)
-                </p>
-                <p className="pb-[31px] pl-[80px] pr-[70px] font-helvetica font-normal xl:text-[12px] leading-[24px] lg:text-[8px]">
-                  2 Years (Master’s)
-                </p>
+                <div className="bg-white">
+                  <p className="pt-[31px] xl:pl-[73px] xl:pr-[63px]  pb-[10px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
+                    4 Years (Bachelor’s)
+                  </p>
+                  <p className="pb-[31px] pl-[80px] pr-[70px] font-helvetica font-normal xl:text-[12px] leading-[24px] lg:text-[8px]">
+                    2 Years (Master’s)
+                  </p>
+                </div>
               </div>
               {/* Language Proficiency */}
               <div className=" text-center ">
@@ -40,25 +42,29 @@ const Glance = () => {
                 >
                   Language Proficiency
                 </h3>
-                <p className="pt-[31px] pl-[63px] pr-[53px] font-helvetica font-normal xl:text-[12px] 2xl:text-[12px] lg:text-[10px] leading-[24px]">
-                  IELTS- 6+, TOEFL-70+
-                </p>
-                <p className="pt-[10px] pb-[31px] pl-[18px] pr-[50.5] font-helvetica font-normal text-[12px] leading-[24px]">
-                  Duolingo-90+, PTE-50+
-                </p>
+                <div className="bg-white">
+                  <p className="pt-[31px] pl-[63px] pr-[53px] font-helvetica font-normal xl:text-[12px] 2xl:text-[12px] lg:text-[10px] leading-[24px]">
+                    IELTS- 6+, TOEFL-70+
+                  </p>
+                  <p className="pt-[10px] pb-[31px] pl-[18px] pr-[50.5] font-helvetica font-normal text-[12px] leading-[24px]">
+                    Duolingo-90+, PTE-50+
+                  </p>
+                </div>
               </div>
               {/* Air Ticket */}
               <div className=" text-center">
                 <h3
-                  className="py-[23.5px] px-[99px] font-bold  xl:text-[12px] 
-                2xl:text-[14px] lg:text-[5px] 
+                  className="py-[23.5px] px-[99px] font-bold  xl:text-[14px] 
+                 lg:text-[5px] 
             leading-[23px] bg-[#F6941E] text-white"
                 >
                   Air Ticket
                 </h3>
-                <p className="py-[48px] font-helvetica font-normal text-[12px] leading-[24px] ">
-                  100000 To 150000 (BDT)
-                </p>
+                <div className="bg-white">
+                  <p className="xl:py-[41px]  lg:py-[41px] font-helvetica font-normal text-[12px] leading-[24px] ">
+                    100000 To 150000 (BDT)
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -70,12 +76,14 @@ const Glance = () => {
                 <h3 className="pl-[113.5px] pr-[113.5px] py-[23.5px] font-bold text-[14px] leading-[23px] bg-[#F6941E] text-white">
                   Intake
                 </h3>
-                <p className="pt-[31px] pl-[90px] pr-[80px] pb-[10px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
-                  January, May,
-                </p>
-                <p className="lg:pb-[31px] xl:pb-[31px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
-                  September
-                </p>
+                <div className="bg-white">
+                  <p className="pt-[31px] pl-[90px] pr-[80px] pb-[10px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
+                    January, May,
+                  </p>
+                  <p className="lg:pb-[31px] xl:pb-[31px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
+                    September
+                  </p>
+                </div>
               </div>
               {/* Tuition Fee (Yearly) */}
               <div className=" text-center">
@@ -86,9 +94,11 @@ const Glance = () => {
                 >
                   Tuition Fee (Yearly)
                 </h3>
-                <p className="py-[48px] pl-[63px] pr-[53px] font-helvetica font-normal xl:text-[12px] 2xl:text-[12px] lg:text-[10px] leading-[24px]">
-                  10000 To 55000 (USD)
-                </p>
+                <div className="bg-white">
+                  <p className="py-[48px] pl-[63px] pr-[53px] font-helvetica font-normal xl:text-[12px] 2xl:text-[12px] lg:text-[10px] leading-[24px]">
+                    10000 To 55000 (USD)
+                  </p>
+                </div>
               </div>
               {/* Processing Time */}
               <div className=" text-center">
@@ -99,9 +109,11 @@ const Glance = () => {
                 >
                   Processing Time
                 </h3>
-                <p className="py-[41px] font-helvetica font-normal text-[12px] leading-[24px]">
-                  9-12 Months
-                </p>
+                <div className="bg-white">
+                  <p className="py-[41px] font-helvetica font-normal text-[12px] leading-[24px]">
+                    9-12 Months
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -112,9 +124,11 @@ const Glance = () => {
                 <h3 className=" py-[23.5px] px-[72.5px] font-bold xl:text-[14px]  lg:text-[10px] leading-[23px] bg-[#F6941E] text-white">
                   Application Fee
                 </h3>
-                <p className="pt-[48px] xl:pb-[48px] lg:pb-[48px] pb-[97px] pl-[85px] pr-[75px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
-                  40 To 200 (USD)
-                </p>
+                <div className="bg-white">
+                  <p className="pt-[48px] xl:pb-[48px] lg:pb-[48px] pb-[97px] pl-[85px] pr-[75px] font-helvetica font-normal xl:text-[12px]  lg:text-[8px] leading-[24px]">
+                    40 To 200 (USD)
+                  </p>
+                </div>
               </div>
 
               {/* Living Cost (Yearly) */}
@@ -126,13 +140,15 @@ const Glance = () => {
                 >
                   Living Cost (Yearly)
                 </h3>
-                <p
-                  className="py-[48px] pl-[63px] pr-[53px] font-normal xl:text-[12px]
+                <div className="bg-white">
+                  <p
+                    className="py-[48px] pl-[63px] pr-[53px] font-normal xl:text-[12px]
                   2xl:text-[12px] lg:text-[10px]
             leading-[24px] "
-                >
-                  10000 To 18000 (USD)
-                </p>
+                  >
+                    10000 To 18000 (USD)
+                  </p>
+                </div>
               </div>
 
               {/* Part Time Work */}
@@ -144,9 +160,11 @@ const Glance = () => {
                 >
                   Part Time Work
                 </h3>
-                <p className="py-[41px] font-helvetica font-normal text-[12px] leading-[24px]">
-                  20 Hrs (per Week)
-                </p>
+                <div className="bg-white">
+                  <p className="py-[41px] font-helvetica font-normal text-[12px] leading-[24px]">
+                    20 Hrs (per Week)
+                  </p>
+                </div>
               </div>
             </div>
 
@@ -157,18 +175,20 @@ const Glance = () => {
                 <h3 className="px-[80.5px] py-[23.5px] font-bold text-[14px] leading-[23px] bg-[#F6941E] text-white">
                   Requirements
                 </h3>
-                <p
-                  className="pt-[34px] pl-[40px] pr-[30px] font-normal font-helvetica xl:text-[12px]
+                <div className="bg-white">
+                  <p
+                    className="pt-[34px] pl-[40px] pr-[30px] font-normal font-helvetica xl:text-[12px]
                 lg:text-[8px] leading-[25.6px] pb-[1px]"
-                >
-                  GRE/GMAT + IELTS/TOEFL/
-                </p>
-                <p
-                  className="pb-[33.5px] pl-[40px] pr-[30px]  font-normal font-helvetica xl:text-[12px]
+                  >
+                    GRE/GMAT + IELTS/TOEFL/
+                  </p>
+                  <p
+                    className="pb-[33.5px] pl-[40px] pr-[30px]  font-normal font-helvetica xl:text-[12px]
                 lg:text-[8px] leading-[25.6px]"
-                >
-                  PTE, Academics: Above 55%
-                </p>
+                  >
+                    PTE, Academics: Above 55%
+                  </p>
+                </div>
               </div>
 
               {/* Visa Application */}
@@ -180,12 +200,14 @@ const Glance = () => {
                 >
                   Visa Application
                 </h3>
-                <p className=" pt-[29px] pl-[40px] pr-[30px] font-normal font-helvetica text-[12px] leading-[25.6px] pb-[10px]">
-                  160 (USD)
-                </p>
-                <p className="pb-[29.5px] pl-[40px] pr-[30px] font-normal font-helvetica text-[12px] leading-[25.6px] ">
-                  SEVIS Fee 350 (USD)
-                </p>
+                <div className="bg-white">
+                  <p className=" pt-[29px] pl-[40px] pr-[30px] font-normal font-helvetica text-[12px] leading-[25.6px] pb-[10px]">
+                    160 (USD)
+                  </p>
+                  <p className="pb-[29.5px] pl-[40px] pr-[30px] font-normal font-helvetica text-[12px] leading-[25.6px] ">
+                    SEVIS Fee 350 (USD)
+                  </p>
+                </div>
               </div>
               {/* Post Study Work Permit */}
               <div className="text-center">
@@ -195,10 +217,12 @@ const Glance = () => {
                 >
                   Post Study Work Permit
                 </h3>
-                <p className="pl-[20px] pr-[10px] py-[8px] font-helvetica font-normal text-[12px] leading-[24px]">
-                  12 Months for All Programs 12+ 24 Months Extension for STEM
-                  (Science, Tech, Engg & Maths)
-                </p>
+                <div className="bg-white pb-[18px]">
+                  <p className="pl-[20px] pr-[10px] py-[8px] font-helvetica font-normal text-[12px] leading-[24px]">
+                    12 Months for All Programs 12+ 24 Months Extension for STEM
+                    (Science, Tech, Engg & Maths)
+                  </p>
+                </div>
               </div>
             </div>
           </div>
@@ -218,12 +242,14 @@ const Glance = () => {
               >
                 Course Duration
               </h3>
-              <p className="pt-[31px] pl-[73px] pr-[63px]font-normal  font-helvetica  xs:text-[12px] pb-[10px]   ">
-                4 Years (Bachelor’s)
-              </p>
-              <p className="pb-[31px] pl-[80px] pr-[70px] font-normal  font-helvetica  xs:text-[12px]   ">
-                2 Years (Master’s)
-              </p>
+              <div className="bg-white">
+                <p className="pt-[31px] pl-[73px] pr-[63px]font-normal  font-helvetica  xs:text-[12px] pb-[10px]   ">
+                  4 Years (Bachelor’s)
+                </p>
+                <p className="pb-[31px] pl-[80px] pr-[70px] font-normal  font-helvetica  xs:text-[12px]   ">
+                  2 Years (Master’s)
+                </p>
+              </div>
             </div>
             {/* intak */}
             <div className="  text-center ">
@@ -233,12 +259,14 @@ const Glance = () => {
               >
                 Intake
               </h3>
-              <p className="pt-[31px] pl-[95px] pr-[85px] pb-[10px] font-normal  font-helvetica  xs:text-[12px]  ">
-                January, May,{" "}
-              </p>
-              <p className="pb-[31px] font-normal  font-helvetica  xs:text-[12px] ">
-                September
-              </p>
+              <div className="bg-white">
+                <p className="pt-[31px] pl-[95px] pr-[85px] pb-[10px] font-normal  font-helvetica  xs:text-[12px]  ">
+                  January, May,{" "}
+                </p>
+                <p className="pb-[31px] font-normal  font-helvetica  xs:text-[12px] ">
+                  September
+                </p>
+              </div>
             </div>
             {/* Application Fee */}
             <div className="  text-center ">
@@ -248,9 +276,11 @@ const Glance = () => {
               >
                 Application Fee
               </h3>
-              <p className="py-[48px] pl-[85px] pr-[75px] font-normal  font-helvetica  xs:text-[12px] ">
-                40 To 200 (USD)
-              </p>
+              <div className="bg-white">
+                <p className="py-[48px] pl-[85px] pr-[75px] font-normal  font-helvetica  xs:text-[12px] ">
+                  40 To 200 (USD)
+                </p>
+              </div>
             </div>
             {/* Requirements */}
             <div className=" text-center">
@@ -260,12 +290,14 @@ const Glance = () => {
               >
                 Requirements
               </h3>
-              <p className="pt-[34px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] pb-[1px]">
-                GRE/GMAT + IELTS/TOEFL/
-              </p>
-              <p className="pb-[34px] pl-[40px] pr-[30px]  font-normal font-helvetica   xs:text-[12px] leading-[25.6px]">
-                PTE, Academics: Above 55%
-              </p>
+              <div className="bg-white">
+                <p className="pt-[34px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] pb-[1px]">
+                  GRE/GMAT + IELTS/TOEFL/
+                </p>
+                <p className="pb-[34px] pl-[40px] pr-[30px]  font-normal font-helvetica   xs:text-[12px] leading-[25.6px]">
+                  PTE, Academics: Above 55%
+                </p>
+              </div>
             </div>
             {/* Language Proficiency */}
             <div className=" text-center ">
@@ -275,12 +307,14 @@ const Glance = () => {
               >
                 Language Proficiency
               </h3>
-              <p className="pt-[31px] pl-[63px] pr-[53px] font-normal  font-helvetica  xs:text-[12px] ">
-                IELTS- 6+, TOEFL-70+
-              </p>
-              <p className="pt-[10px] pb-[31px] pl-[60.5px] pr-[50.5] font-normal  font-helvetica  xs:text-[12px] ">
-                Duolingo-90+, PTE-50+
-              </p>
+              <div className="bg-white">
+                <p className="pt-[31px] pl-[63px] pr-[53px] font-normal  font-helvetica  xs:text-[12px] ">
+                  IELTS- 6+, TOEFL-70+
+                </p>
+                <p className="pt-[10px] pb-[31px] pl-[60.5px] pr-[50.5] font-normal  font-helvetica  xs:text-[12px] ">
+                  Duolingo-90+, PTE-50+
+                </p>
+              </div>
             </div>
 
             {/* Tuition Fee (Yearly) */}
@@ -291,9 +325,11 @@ const Glance = () => {
               >
                 Tuition Fee (Yearly)
               </h3>
-              <p className="py-[48px] pl-[63px] pr-[53px]  font-normal  font-helvetica  xs:text-[12px] ">
-                10000 To 55000 (USD)
-              </p>
+              <div className="bg-white">
+                <p className="py-[48px] pl-[63px] pr-[53px]  font-normal  font-helvetica  xs:text-[12px] ">
+                  10000 To 55000 (USD)
+                </p>
+              </div>
             </div>
             {/* Living Cost (Yearly) */}
 
@@ -304,12 +340,14 @@ const Glance = () => {
               >
                 Living Cost (Yearly)
               </h3>
-              <p
-                className="py-[48px] pl-[63px] pr-[53px] font-normal  font-helvetica  xs:text-[12px]
+              <div className="bg-white">
+                <p
+                  className="py-[48px] pl-[63px] pr-[53px] font-normal  font-helvetica  xs:text-[12px]
             leading-[24px] "
-              >
-                10000 To 18000 (USD)
-              </p>
+                >
+                  10000 To 18000 (USD)
+                </p>
+              </div>
             </div>
             {/* Visa Application */}
             <div className=" text-center">
@@ -319,12 +357,14 @@ const Glance = () => {
               >
                 Visa Application
               </h3>
-              <p className=" pt-[29px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] pb-[10px]">
-                160 (USD)
-              </p>
-              <p className="pb-[29.5px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] ">
-                SEVIS Fee 350 (USD)
-              </p>
+              <div className="bg-white">
+                <p className=" pt-[29px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] pb-[10px]">
+                  160 (USD)
+                </p>
+                <p className="pb-[29.5px] pl-[40px] pr-[30px] font-normal  font-helvetica  xs:text-[12px] leading-[25.6px] ">
+                  SEVIS Fee 350 (USD)
+                </p>
+              </div>
             </div>
             {/* Air Ticket */}
             <div className=" text-center">
@@ -334,9 +374,11 @@ const Glance = () => {
               >
                 Air Ticket
               </h3>
-              <p className="py-[41px] font-normal  font-helvetica  xs:text-[12px] ">
-                100000 To 150000 (BDT)
-              </p>
+              <div className="bg-white">
+                <p className="py-[41px]  font-normal  font-helvetica  xs:text-[12px] ">
+                  100000 To 150000 (BDT)
+                </p>
+              </div>
             </div>
 
             {/* row2 */}
@@ -349,9 +391,11 @@ const Glance = () => {
               >
                 Processing Time
               </h3>
-              <p className="py-[41px] font-normal  font-helvetica  xs:text-[12px] ">
-                9-12 Months
-              </p>
+              <div className="bg-white">
+                <p className="py-[41px] font-normal  font-helvetica  xs:text-[12px] ">
+                  9-12 Months
+                </p>
+              </div>
             </div>
 
             {/* row3 */}
@@ -364,9 +408,11 @@ const Glance = () => {
               >
                 Part Time Work
               </h3>
-              <p className="py-[41px] font-normal  font-helvetica  xs:text-[12px] ">
-                20 Hrs (per Week)
-              </p>
+              <div className="bg-white">
+                <p className="py-[41px] font-normal  font-helvetica  xs:text-[12px] ">
+                  20 Hrs (per Week)
+                </p>
+              </div>
             </div>
 
             {/* row4 */}
@@ -379,10 +425,12 @@ const Glance = () => {
               >
                 Post Study Work Permit
               </h3>
-              <p className="pl-[20px] pr-[10px] py-[8px] font-normal  font-helvetica  xs:text-[12px] ">
-                12 Months for All Programs 12+ 24 Months Extension for STEM
-                (Science, Tech, Engg & Maths)
-              </p>
+              <div className="bg-white">
+                <p className="pl-[20px] pr-[10px] py-[8px] font-normal  font-helvetica  xs:text-[12px] ">
+                  12 Months for All Programs 12+ 24 Months Extension for STEM
+                  (Science, Tech, Engg & Maths)
+                </p>
+              </div>
             </div>
           </div>
         </div>
