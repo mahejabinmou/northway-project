4e409d0683a78972e649b9c4ad30617e7f73fb60
dmemopract <dmemopract@gmail.com>
2024-10-27 17:37:09 +0600

    mission and vission added

--- src/ClientSide/AboutUs/AboutUs.jsx
+++ src/ClientSide/AboutUs/AboutUs.jsx
@@ -2,16 +2,21 @@ import OurService from "../Home/OurService/OurService";
 import OurStudentSay from "../Home/OurStudentSay/OurStudentSay";
 import Footer from "../Shared/Footer/Footer";
 import AboutUsHero from "./AboutUsHero/AboutUsHero";
+import Mission from "./Mission/Mission";
+import Vision from "./Vision/Vision";
 import WhoAreU from "./WhoAreU/WhoAreU";
 
 const AboutUs = () => {
   return (
     <div className="bg-[#F5F5F5]">
       <AboutUsHero></AboutUsHero>
-      {/* Here you new component for mission and vision */}
+
       <WhoAreU></WhoAreU>
       <OurService></OurService>
       <OurStudentSay></OurStudentSay>
+      {/* Here you new component for mission and vision */}
+      <Mission></Mission>
+      <Vision></Vision>
       <Footer></Footer>
     </div>
   );
---
+++ src/ClientSide/AboutUs/Mission/Mission.jsx
@@ -0,0 +1,5 @@
+const Mission = () => {
+  return <div></div>;
+};
+
+export default Mission;
